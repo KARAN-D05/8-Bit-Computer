b99dbf2b25deabb2aa0b3253f0de0eb2822758eb
Karan Diwan <karandiwan46@gmail.com>
2026-06-24 03:42:23 +0530

    8-Bit Parameterized ALU - 8 Operations, 5 flags

---
+++ ALU/src/ALU.v
@@ -0,0 +1,48 @@
+module ALU #(
+    parameter WIDTH = 8,
+    parameter NUM_OPS = 8
+) (
+    input [WIDTH-1:0] a,
+    input [WIDTH-1:0] b,
+    input [$clog2(NUM_OPS)-1:0] sel,
+    output reg [WIDTH-1:0] out,
+    output neg,
+    output zero,
+    output agtb,
+    output aeqb,
+    output reg carry
+);
+   
+  wire [WIDTH:0] add, sub;
+  assign add = a + b;
+  assign sub = a - b; 
+
+  assign agtb = (a > b) ? 1 : 0;
+  assign aeqb = (a == b) ? 1 : 0;
+  assign zero = ~(|out);
+  assign neg = out[WIDTH-1];
+
+  always@(*) begin
+
+    carry = 0;
+
+    case (sel)
+    3'b000 : begin 
+        out = add[WIDTH-1:0];
+        carry = add[WIDTH];
+    end
+    3'b001 : begin
+        out = sub[WIDTH-1:0];
+        carry = sub[WIDTH];
+     end
+    3'b010 : out = a & b;
+    3'b011 : out = a | b;
+    3'b100 : out = a ^ b;
+    3'b101 : out = ~a;
+    3'b110 : out = a;
+    3'b111 : out = b;
+    default : out = a + b;
+    endcase
+  end
+
+endmodule
---
+++ ALU/src/ALU_tb.v
@@ -0,0 +1,113 @@
+`timescale 1ns/1ns
+
+module testbench;
+
+  parameter WIDTH = 8;
+  parameter NUM_OPS = 8;
+
+  reg [WIDTH-1:0] a;
+  reg [WIDTH-1:0] b;
+  reg [$clog2(NUM_OPS)-1:0] sel;
+  wire [WIDTH-1:0] out;
+  wire neg;
+  wire zero;
+  wire agtb;
+  wire aeqb;
+  wire carry;
+
+  ALU #(
+    .WIDTH(WIDTH),
+    .NUM_OPS(NUM_OPS)
+) dut (
+    .a(a),
+    .b(b),
+    .sel(sel),
+    .out(out),
+    .neg(neg),
+    .zero(zero),
+    .agtb(agtb),
+    .aeqb(aeqb),
+    .carry(carry)
+  );
+
+  initial begin
+
+   $monitor("time = %0t | a = %b | b = %b | sel = %b | out = %b | neg = %b | zero = %b | agtb = %b | aeqb = %b | carry = %b"
+   , $time, a, b, sel, out, neg, zero, agtb, aeqb, carry);
+
+   $dumpfile("Sim.vcd");
+   $dumpvars(0, testbench);
+   
+   // ADD (3+1)
+   a = 8'b00000011;
+   b = 8'b00000001;
+   sel = 3'b000;
+   #5;
+
+   // ADD (255+1)
+   a = 8'b11111111;
+   b = 8'b00000001;
+   sel = 3'b000;
+   #5;
+
+   // SUB (3-1)
+   a = 8'b00000011;
+   b = 8'b00000001;
+   sel = 3'b001;
+   #5;
+
+   // SUB (3-7)
+   a = 8'b00000011;
+   b = 8'b00000111;
+   sel = 3'b001;
+   #5;
+
+   // SUB (0-1)
+   a = 8'b00000000;
+   b = 8'b00000001;
+   sel = 3'b001;
+   #5;
+   
+   // AND 
+   a = 8'b11100110;
+   b = 8'b01111111;
+   sel = 3'b010;
+   #5;
+  
+   // OR
+   a = 8'b11100110;
+   b = 8'b01111111;
+   sel = 3'b011;
+   #5;
+
+   // XOR
+   a = 8'b11100110;
+   b = 8'b01111111;
+   sel = 3'b100;
+   #5;
+   
+   // NOT a
+   a = 8'b11111111;
+   b = 8'b11111111;
+   sel = 3'b101;
+   #5;
+
+   // PASS a
+   a = 8'b11100110;
+   b = 8'b01111111;
+   sel = 3'b110;
+   #5;
+
+   // PASS b
+   a = 8'b11100110;
+   b = 8'b01111111;
+   sel = 3'b111;
+   #5;
+
+
+   $display("Simulation Complete!");
+   $finish;
+
+  end
+
+endmodule
