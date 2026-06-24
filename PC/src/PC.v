0c46957d9ae6f6c00134507f692d91b65e060cbe
Karan Diwan <karandiwan46@gmail.com>
2026-06-24 06:33:35 +0530

    Parameterized Program Counter

---
+++ PC/src/PC.v
@@ -0,0 +1,23 @@
+module PC #(
+    parameter WIDTH = 8
+) (
+    input [WIDTH-1:0] in,
+    input load,
+    input clk,
+    input rst,
+    output reg [WIDTH-1:0] out
+);
+
+ always@(posedge clk or posedge rst )begin
+    
+    if (rst) begin
+        out <= 8'b0;
+    end else if (load) begin 
+        out <= in;
+    end else begin
+        out <= out + 1;
+    end
+
+ end
+
+endmodule
---
+++ PC/src/PC_tb.v
@@ -0,0 +1,100 @@
+`timescale 1ns/1ns
+
+module testbench;
+
+  parameter WIDTH = 8;
+
+  reg [WIDTH-1:0] in;
+  reg load;
+  reg rst;
+  reg clk;
+  wire [WIDTH-1:0] out;
+
+  PC # ( .WIDTH(WIDTH)
+  ) dut (
+    .in(in),
+    .load(load),
+    .rst(rst),
+    .clk(clk),
+    .out(out)
+  );
+
+  initial clk = 0;
+  always #5 clk = ~clk;
+
+  initial begin
+
+  $monitor("t = %0t | out = %b | in = %b | load = %b | rst = %b", $time, out, in, load, rst);
+
+  $dumpfile("Sim.vcd");
+  $dumpvars(0, testbench);
+
+  in   = 8'h00;
+  load = 0;
+  rst  = 1;
+
+  // Reset PC
+  @(negedge clk);
+  #1;
+  rst = 0;
+
+  // PC should increment
+  @(negedge clk);
+  #1;
+
+  @(negedge clk);
+  #1;
+
+  @(negedge clk);
+  #1;
+
+  // Jump to 0x55
+  in   = 8'h55;
+  load = 1;
+
+  @(negedge clk);
+  #1;
+  load = 0;
+
+  // Continue counting from 0x55
+  @(negedge clk);
+  #1;
+
+  @(negedge clk);
+  #1;
+
+  // Jump to 0xA0
+  in   = 8'hA0;
+  load = 1;
+
+  @(negedge clk);
+  #1;
+  load = 0;
+
+  // Continue counting from 0xA0
+  @(negedge clk);
+  #1;
+
+  @(negedge clk);
+  #1;
+
+  // Test wrap-around
+  in   = 8'hFF;
+  load = 1;
+
+  @(negedge clk);
+  #1;
+  load = 0;
+
+  @(negedge clk);
+  #1;
+
+  @(posedge clk);
+  #1;
+
+  $display("Simulation Complete!");
+  $finish;
+
+ end
+
+endmodule
